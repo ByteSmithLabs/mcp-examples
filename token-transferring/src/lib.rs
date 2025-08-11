2d6d632ae59d354ce5285806adcf708af16310bf
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-08-11 20:18:25 +0700

    Support multi tenant (#7)
    * switch to oauth
    
    * use client sdk
    
    * update transfer and get_balance
    
    * update get_account_address

--- token-transferring/src/lib.rs
+++ token-transferring/src/lib.rs
@@ -1,25 +1,43 @@
+use candid::CandidType;
+use candid::{Nat, Principal};
 use ic_cdk::api;
 use ic_cdk::{init, query, update};
 use ic_http_certification::{HttpRequest, HttpResponse, StatusCode};
-use ic_rmcp::{model::*, schema_for_type, Context, Error, Handler, Server};
+use ic_rmcp::{
+    model::*, schema_for_type, Context, Error, Handler, IssuerConfig, OAuthConfig, Server,
+};
+use icrc_ledger_client::ICRC1Client;
+use icrc_ledger_types::icrc1::{
+    account::{principal_to_subaccount, Account},
+    transfer::TransferArg,
+};
 use rust_decimal::Decimal;
 use schemars::JsonSchema;
 use serde::Deserialize;
 use serde_json::{from_value, Value};
 use std::cell::RefCell;
 
-use crate::ledger::{Account, Service, TransferArg};
-use candid::{Nat, Principal};
+mod runtime;
 
-mod ledger;
+use runtime::CdkRuntime;
 
 thread_local! {
     static TOKENS: RefCell<Vec<(String, String, u8)>> = const{RefCell::new(Vec::new())};
-    static API_KEY : RefCell<String> = const {RefCell::new(String::new())} ;
+    static ARGS : RefCell<InitArgs> =  RefCell::default();
+}
+
+#[derive(Deserialize, CandidType, Default)]
+struct InitArgs {
+    metadata_url: String,
+    resource: String,
+    issuer: String,
+    jwks_url: String,
+    authorization_server: Vec<String>,
+    audience: String,
 }
 
 #[init]
-fn init(api_key: String) {
+fn init(config: InitArgs) {
     TOKENS.with_borrow_mut(|tokens| {
         tokens.push((
             "ICP".to_string(),
@@ -43,7 +61,7 @@ fn init(api_key: String) {
         ))
     });
 
-    API_KEY.with_borrow_mut(|key| *key = api_key)
+    ARGS.with_borrow_mut(|args| *args = config);
 }
 
 struct TokenTransferring;
@@ -57,7 +75,6 @@ struct TransferRequest {
 
 #[derive(JsonSchema, Deserialize)]
 struct GetBalanceRequest {
-    owner: Option<String>,
     ledger_canister_id: String,
 }
 
@@ -70,7 +87,7 @@ struct AddTokenRequest {
 impl Handler for TokenTransferring {
     async fn call_tool(
         &self,
-        _: Context,
+        context: Context,
         req: CallToolRequestParam,
     ) -> Result<CallToolResult, Error> {
         match req.name.as_ref() {
@@ -82,33 +99,35 @@ impl Handler for TokenTransferring {
                     Error::invalid_params("invalid arguments to tool get_balance", None)
                 })?;
 
-                let ledger = Service(
-                    Principal::from_text(request.ledger_canister_id)
+                let client = ICRC1Client {
+                    runtime: CdkRuntime,
+                    ledger_canister_id: Principal::from_text(request.ledger_canister_id)
                         .map_err(|_| Error::invalid_params("invalid ledger canister id", None))?,
-                );
-
-                let (symbol,) = ledger
-                    .icrc_1_symbol()
-                    .await
-                    .map_err(|err| Error::internal_error(format!("{err:?}"), None))?;
-
-                let (decimals,) = ledger
-                    .icrc_1_decimals()
-                    .await
-                    .map_err(|err| Error::internal_error(format!("{err:?}"), None))?;
-
-                let owner = if let Some(own_text) = request.owner {
-                    Principal::from_text(own_text).map_err(|_| {
-                        Error::invalid_params("invalid arguments to tool get_balance", None)
-                    })?
-                } else {
-                    api::canister_self()
                 };
 
-                let balance = ledger
-                    .icrc_1_balance_of(&Account {
-                        owner,
-                        subaccount: None,
+                let symbol = client
+                    .symbol()
+                    .await
+                    .map_err(|err| Error::internal_error(format!("{err:?}"), None))?;
+
+                let decimals = client
+                    .decimals()
+                    .await
+                    .map_err(|err| Error::internal_error(format!("{err:?}"), None))?;
+
+                let subject = context
+                    .subject
+                    .ok_or(Error::internal_error("no subject".to_string(), None))?;
+
+                let subaccount = principal_to_subaccount(
+                    Principal::from_text(subject)
+                        .map_err(|err| Error::internal_error(format!("{err:?}"), None))?,
+                );
+
+                let balance = client
+                    .balance_of(Account {
+                        owner: api::canister_self(),
+                        subaccount: Some(subaccount),
                     })
                     .await
                     .map_err(|err| Error::internal_error(format!("{err:?}"), None))?;
@@ -117,7 +136,7 @@ impl Handler for TokenTransferring {
                     Content::text(format!(
                         "The balance is {} {}",
                         Decimal::from_i128_with_scale(
-                            i128::try_from(balance.0 .0).unwrap(),
+                            i128::try_from(balance.0).unwrap(),
                             decimals as u32
                         ),
                         symbol
@@ -135,52 +154,78 @@ impl Handler for TokenTransferring {
                     Error::invalid_params("invalid principal in tool transfer", None)
                 })?;
 
-                let ledger = Service(
-                    Principal::from_text(req.ledger_canister_id)
+                let client = ICRC1Client {
+                    runtime: CdkRuntime,
+                    ledger_canister_id: Principal::from_text(req.ledger_canister_id)
                         .map_err(|_| Error::invalid_params("invalid ledger canister id", None))?,
+                };
+
+                let subject = context
+                    .subject
+                    .ok_or(Error::internal_error("no subject".to_string(), None))?;
+
+                let subaccount = principal_to_subaccount(
+                    Principal::from_text(subject)
+                        .map_err(|err| Error::internal_error(format!("{err:?}"), None))?,
                 );
 
-                let _ = ledger
-                    .icrc_1_transfer(&TransferArg {
+                let _ = client
+                    .transfer(TransferArg {
                         to: Account {
                             owner: recipiant,
                             subaccount: None,
                         },
                         fee: None,
                         memo: None,
-                        from_subaccount: None,
+                        from_subaccount: Some(subaccount),
                         created_at_time: None,
                         amount: Nat::from(req.amount),
                     })
                     .await
                     .map_err(|err| Error::internal_error(format!("{err:?}"), None))?
-                    .0
                     .map_err(|err| Error::internal_error(format!("{err:?}"), None))?;
 
                 Ok(CallToolResult::success(
                     Content::text("Success").into_contents(),
                 ))
             }
-            "get_principal" => Ok(CallToolResult::success(
-                Content::text(format!("Canister principal: {}", api::canister_self()))
+            "get_account_address" => {
+                let subject = context
+                    .subject
+                    .ok_or(Error::internal_error("no subject".to_string(), None))?;
+
+                let subaccount = principal_to_subaccount(
+                    Principal::from_text(subject)
+                        .map_err(|err| Error::internal_error(format!("{err:?}"), None))?,
+                );
+
+                Ok(CallToolResult::success(
+                    Content::text(format!(
+                        "Canister principal: {}",
+                        Account {
+                            owner: api::canister_self(),
+                            subaccount: Some(subaccount),
+                        }
+                    ))
                     .into_contents(),
-            )),
+                ))
+            }
             "add_token" => {
                 let request = from_value::<AddTokenRequest>(Value::Object(req.arguments.ok_or(
                     Error::invalid_params("invalid arguments to tool add_token", None),
                 )?))
                 .map_err(|_| Error::invalid_params("invalid arguments to tool add_token", None))?;
 
-                let ledger = Service(
-                    Principal::from_text(&request.ledger_canister_id)
+                let client = ICRC1Client {
+                    runtime: CdkRuntime,
+                    ledger_canister_id: Principal::from_text(request.ledger_canister_id.clone())
                         .map_err(|_| Error::invalid_params("invalid ledger canister id", None))?,
-                );
+                };
 
-                let decimals = ledger
-                    .icrc_1_decimals()
+                let decimals = client
+                    .decimals()
                     .await
-                    .map_err(|err| Error::internal_error(format!("{err:?}"), None))?
-                    .0;
+                    .map_err(|err| Error::internal_error(format!("{err:?}"), None))?;
 
                 TOKENS.with_borrow_mut(|tokens| {
                     tokens.push((request.name, request.ledger_canister_id, decimals));
@@ -206,13 +251,13 @@ impl Handler for TokenTransferring {
             tools: vec![
                 Tool::new(
                     "get_balance",
-                    "Get ICRC-1 token balance for the given account. It accepts two parameters: textual principal and ledger canister ID. If principal is empty, default to canister (server) account.",
+                    "Get ICRC-1 token balance for your account. The subaccount will be derived from user's authenticated identity.",
                     schema_for_type::<GetBalanceRequest>(),
                 ),
                 Tool::new("transfer",
-                 "Transfer ICRC-1 token from the canister (server) account to the given destination principal. The unit is in decimal format. For example, to transfer 1.5 ICP, you should pass 150000000 as amount. To get decimals value, use get_supported_tokens tool.",
+                 "Transfer ICRC-1 token from your account to the given destination principal. The unit is in decimal format. For example, to transfer 1.5 ICP, you should pass 150000000 as amount. To get decimals value, use get_supported_tokens tool.",
                 schema_for_type::<TransferRequest>()),
-                Tool::new("get_principal", "Get the canister (server) default account in textual principal. Use this tool when you need to top up ICRC-1 token to your canister (server).", schema_for_type::<EmptyObject>()),
+                Tool::new("get_account_address", "Get your address. Basically, it consists of a subaccount mapped from authenticated identity, under the server principal. Use this tool when you need to top up ICRC-1 token to your account.", schema_for_type::<EmptyObject>()),
                 Tool::new("add_token", "Add new token to token list.", schema_for_type::<AddTokenRequest>()),
                 Tool::new("get_supported_tokens", "Return a list of supported tokens. Use this when retrieving token's ledger canister ID and its decimals.", schema_for_type::<EmptyObject>())
             ],
@@ -227,8 +272,8 @@ impl Handler for TokenTransferring {
             },
             instructions: Some(
                 r"
-            This server provides tools to transfer ICRC-1 tokens from the canister account (default 
-            account; represented by the server) to destination account.
+            This server provides tools to transfer ICRC-1 tokens user's account to destination account.
+            Each user will control a subaccount under canister (server) principal. 
             "
                 .to_string(),
             ),
@@ -248,11 +293,20 @@ fn http_request(_: HttpRequest) -> HttpResponse {
 #[update]
 async fn http_request_update(req: HttpRequest<'_>) -> HttpResponse<'_> {
     TokenTransferring {}
-        .handle(&req, |headers| {
-            headers
-                .iter()
-                .any(|(k, v)| k == "x-api-key" && *v == API_KEY.with_borrow(|k| k.clone()))
-        })
+        .handle_with_oauth(
+            &req,
+            ARGS.with_borrow(|args| OAuthConfig {
+                metadata_url: args.metadata_url.clone(),
+                resource: args.resource.clone(),
+                issuer_configs: IssuerConfig {
+                    issuer: args.issuer.clone(),
+                    jwks_url: args.jwks_url.clone(),
+                    authorization_server: args.authorization_server.clone(),
+                    audience: args.audience.clone(),
+                },
+                scopes_supported: vec![],
+            }),
+        )
         .await
 }
 
