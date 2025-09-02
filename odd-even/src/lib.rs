443707d6674a72f0d47b8b767a7f1dcce30270e5
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-09-02 19:34:51 +0700

    initialize odd-even (#9)
    * initialize
    
    * fix ci
    
    * fix lint

---
+++ odd-even/src/lib.rs
@@ -0,0 +1,100 @@
+use candid::CandidType;
+use ic_cdk::{init, query, update};
+use ic_http_certification::{HttpRequest, HttpResponse, StatusCode};
+use ic_rmcp::{model::*, Context, Error, Handler, IssuerConfig, OAuthConfig, Server};
+use serde::Deserialize;
+use std::cell::RefCell;
+
+thread_local! {
+    static ARGS : RefCell<InitArgs> =  RefCell::default();
+}
+
+#[derive(Deserialize, CandidType, Default)]
+struct InitArgs {
+    metadata_url: String,
+    resource: String,
+    issuer: String,
+    jwks_url: String,
+    authorization_server: Vec<String>,
+    audience: String,
+}
+
+#[init]
+fn init(config: InitArgs) {
+    ARGS.with_borrow_mut(|args| *args = config);
+}
+
+struct OddEven;
+
+impl Handler for OddEven {
+    async fn call_tool(
+        &self,
+        _: Context,
+        _: CallToolRequestParam,
+    ) -> Result<CallToolResult, Error> {
+        // match req.name.as_ref() {
+        //      implement this
+        //     _ => Err(Error::invalid_params("not found tool", None)),
+        // }
+
+        Err(Error::invalid_params("not found tool", None))
+    }
+    async fn list_tools(
+        &self,
+        _: Context,
+        _: Option<PaginatedRequestParam>,
+    ) -> Result<ListToolsResult, Error> {
+        Ok(ListToolsResult {
+            next_cursor: None,
+            tools: vec![
+                // Implement this
+            ],
+        })
+    }
+    fn get_info(&self, _: Context) -> ServerInfo {
+        ServerInfo {
+            capabilities: ServerCapabilities::builder().enable_tools().build(),
+            server_info: Implementation {
+                name: "Odd-even".to_string(),
+                version: "1.0.0".to_string(),
+            },
+            instructions: Some(
+                r"
+            This server provides tools for users to play odd-even game. 
+            "
+                .to_string(),
+            ),
+            ..Default::default()
+        }
+    }
+}
+
+#[query]
+fn http_request(_: HttpRequest) -> HttpResponse {
+    HttpResponse::builder()
+        .with_status_code(StatusCode::OK)
+        .with_upgrade(true)
+        .build()
+}
+
+#[update]
+async fn http_request_update(req: HttpRequest<'_>) -> HttpResponse<'_> {
+    OddEven {}
+        .handle_with_oauth(
+            &req,
+            ARGS.with_borrow(|args| OAuthConfig {
+                metadata_url: args.metadata_url.clone(),
+                resource: args.resource.clone(),
+                issuer_configs: IssuerConfig {
+                    issuer: args.issuer.clone(),
+                    jwks_url: args.jwks_url.clone(),
+                    authorization_server: args.authorization_server.clone(),
+                    audience: args.audience.clone(),
+                },
+                scopes_supported: vec![],
+            }),
+        )
+        .await
+}
+
+ic_cdk::export_candid!();
